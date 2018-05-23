1df0750534283a135e6ef773b0edb76519d00369
coldJune <785691557@qq.com>
2018-05-23 17:16:52 +0800

    添加ROC曲线绘制

--- MachineLearningInAction/adaboost/adaboost.py
+++ MachineLearningInAction/adaboost/adaboost.py
@@ -93,29 +93,29 @@ def adaboost_train_ds(data_arr, class_labels, num_it=40):
     for i in range(num_it):
         # 建立单层决策树
         best_stump, error, class_est = build_stump(data_arr, class_labels, d)
-        print("D:", d.T)
+        # print("D:", d.T)
         # 计算每一个单层决策树的权重
         # max(error,1e-16)保证在没有错误时不会除0异常
         alpha = float(0.5*np.log((1-error)/np.longfloat(max(error, 1e-16))))
         # 保存决策树权重和单层决策树
         best_stump['alpha'] = alpha
         weak_class_arr.append(best_stump)
-        print("class_est:", class_est.T)
+        # print("class_est:", class_est.T)
         # 计算新的权重向量d
         expon = np.multiply(-1*alpha*np.mat(class_labels).T, class_est)
         d = np.multiply(d, np.exp(expon))
         d = d/d.sum()
         # 类别估计值
         agg_class_est += alpha*class_est
-        print('agg_class_est', agg_class_est.T)
+        # print('agg_class_est', agg_class_est.T)
         # 获取错误率
         agg_errors = np.multiply(np.sign(agg_class_est) != np.mat(class_labels).T, np.ones((m, 1)))
-        print(agg_errors)
+        # print(agg_errors)
         error_rate = agg_errors.sum()/m
-        print('total error', error_rate, '\n')
+        # print('total error', error_rate, '\n')
         if error_rate == 0.0:
             break
-    return weak_class_arr
+    return weak_class_arr, agg_class_est
 
 
 def ada_classify(dat_to_class, classifier_arr):
@@ -159,4 +159,55 @@ def load_data_set(file_name):
             # 读取类别
             label_mat.append(float(cur_line[-1]))
     return data_mat, label_mat
+
+
+def plot_roc(pred_strengths, class_labels):
+    """画出ROC曲线
+    :param pred_strengths: 行向量组成的矩阵，表示分类器的预测强度
+    :param class_labels: 类别标签
+    :return:
+    """
+    import matplotlib.pyplot as plt
+    # 保留绘制光标的位置
+    cur = (1.0, 1.0)
+    # 用于计算AUC的值
+    y_sum = 0.0
+    # 计算正例的数目
+    num_pos_class = sum(np.array(class_labels) == 1.0)
+    # 确定x轴和y轴上的步长
+    y_step = 1/float(num_pos_class)
+    x_step = 1/float(len(class_labels)-num_pos_class)
+    # 得到排序索引
+    # 因为索引时按照最小到最大的顺序排列
+    # 所以从点<1.0,1.0>绘到<0,0>
+    sorted_indicies = pred_strengths.argsort()
+    fig = plt.figure()
+    fig.clf()
+    ax = fig.add_subplot(111)
+    for index in sorted_indicies.tolist()[0]:
+        # 在排序值上进行循环
+        if class_labels[index] == 1.0:
+            # 得到一个标签为1.0的类，在y轴上下降一个步长
+            # 即不断降低真阳率
+            del_x = 0
+            del_y = y_step
+        else:
+            # 其他类别的标签，按x轴方向上倒退一个步长
+            # 假阴率方向
+            del_x = x_step
+            del_y = 0
+            # 对矩形的高度进行累加
+            y_sum += cur[1]
+        # 在当前点和新点之间画一条线
+        ax.plot([cur[0], cur[0]-del_x], [cur[1], cur[1]-del_y], color='b')
+        # 更新当前点的位置
+        cur = (cur[0]-del_x, cur[1]-del_y)
+    ax.plot([0, 1], [0, 1], 'b--')
+    plt.xlabel('假阳率')
+    plt.ylabel('真阳率')
+    plt.title('AdaBoost马疝病监测系统的ROC曲线')
+    ax.axis([0, 1, 0, 1])
+    plt.show()
+    # 乘以x_step得到总面积
+    print("曲线下面积为:", y_sum*x_step)
 
